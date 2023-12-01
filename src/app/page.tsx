47e5b03a6925c0318baee824598c30e91d28e385
MuriloCode7 <murilomagalhaes60@gmail.com>
2023-12-01 13:25:32 -0300

    chore: testing git hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-  const test = 'a';
+  const test = 'ab';
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Hello World!</h2>
