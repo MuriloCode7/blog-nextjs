e0cf3ee18dc3c6fe2fe4d6473d2acb7ecb1ac3a8
MuriloCode7 <murilomagalhaes60@gmail.com>
2023-12-01 13:51:17 -0300

    feat: config contentlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { allPosts } from 'contentlayer/generated'
+
 export default function Home() {
-  const test = 'ab';
+  const posts = allPosts.map(post => post)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Hello World!</h2>
+
+      <div dangerouslySetInnerHTML={{__html:posts[0]?.title}}/>
     </main>
   )
 }
